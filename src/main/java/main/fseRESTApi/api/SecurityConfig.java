8d4d76e7b68500fb9dabff970f2f92ea28bc375a
Tim Fuchs <75135582+FixFaxt@users.noreply.github.com>
2024-12-15 13:53:54 +0100

    Remove Users

--- src/main/java/main/fseRESTApi/api/SecurityConfig.java
+++ src/main/java/main/fseRESTApi/api/SecurityConfig.java
@@ -1,21 +1,12 @@
 package main.fseRESTApi.api;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import main.fseRESTApi.api.model.User;
-import main.fseRESTApi.api.service.UserService;
 
 @Configuration
 @EnableWebSecurity
@@ -34,21 +25,5 @@ public class SecurityConfig {
         .httpBasic(basic -> basic.disable());
 
     return http.build();
-  }
-
-  @Bean
-  public UserDetailsService userDetailsService(UserService userService) {
-    return email -> {
-      Optional<User> user = userService.findByEmail(email);
-      if (user.isEmpty()) {
-        throw new UsernameNotFoundException("User not found");
-      }
-
-      User existingUser = user.get();
-      return new org.springframework.security.core.userdetails.User(
-          existingUser.getEmail(),
-          existingUser.getPassword(),
-          List.of(new SimpleGrantedAuthority("ROLE_" + existingUser.getRole().name())));
-    };
   }
 }
